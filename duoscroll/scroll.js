3a7b5a36d0239c8bcb8f2bca47b814ec1b3e4002
Alex Shovlin <ashovlin@gmail.com>
2017-05-02 21:26:41 -0500

    Updating DOM parsing to match new Duolingo structure

--- duoscroll/scroll.js
+++ duoscroll/scroll.js
@@ -1,14 +1,17 @@
 function scrollToFirstUnlockedSkill() {
 
-  var unlockedSkills = document.getElementsByClassName("unlocked");
+  var unlockedSkills = document.getElementsByClassName("W1dac");
 
   for (skill of unlockedSkills) {
-  	// Skip the unlockable skills in the bonus row for now
-  	if (!skill.parentElement.parentElement.parentElement.className.includes("bonus-row")) {
-  		skill.scrollIntoViewIfNeeded(true);
-  		return;
-  	}
-  }
+  		var dataset = skill.dataset.test;
+
+  		// don't include the bonus row (purple) for now
+  		if (dataset.includes("red")|| dataset.includes("green"))
+  		{
+	  		skill.scrollIntoViewIfNeeded(true);
+	  		return;  			
+  		}
+	}
 }
 
 scrollToFirstUnlockedSkill();
